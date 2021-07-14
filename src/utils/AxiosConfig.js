ae4b1da7aa8a551f1920050855eedce04334bc8f
Dinesh I <dineshiyyandhurai@gmail.com>
2021-07-14 12:22:55 +0530

    added live backend server

--- src/utils/AxiosConfig.js
+++ src/utils/AxiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080/api/v1';
+axios.defaults.baseURL = 'https://thankme-backend.herokuapp.com/api/v1';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 
