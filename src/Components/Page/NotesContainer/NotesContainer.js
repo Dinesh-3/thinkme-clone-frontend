87c3499cb1acd371431a04475a275f608d642506
Dinesh I <dineshiyyandhurai@gmail.com>
2021-07-22 11:04:31 +0530

    bug fix in comments

--- src/Components/Page/NotesContainer/NotesContainer.js
+++ src/Components/Page/NotesContainer/NotesContainer.js
@@ -5,27 +5,28 @@ const NotesContainer = ({ notes = [], pageDetail }) => {
 	const [scrollType, setScrollType] = useState('overflowY-hidden');
   
 	return (
-		<div className='d-flex flex-column'>
+		<div className="d-flex flex-column">
 			<p>
 				🎉 {notes.length} people thanked {pageDetail.page_title} for being kind!
 			</p>
 			<div className={`card-container ${scrollType}`}>
-				{notes.map((item) => (
+				{notes.map((item, index) => (
 					<div
-						className='note-card'
+						className="note-card"
 						style={{
 							backgroundColor: `hsl(${item.color}, 50%, 50%)`,
 						}}
+						key={index}
 					>
 						<p>{item.content}</p>
 					</div>
 				))}
 			</div>
 			<button
-				type='button'
-				className='btn btn-light'
-				style={{ borderColor: '#0057DA', color: '#0057DA', margin: '1rem', width: '10rem' }}
-				onClick={() => setScrollType('overflowY-scroll')}
+				type="button"
+				className="btn btn-light"
+				style={{ borderColor: "#0057DA", color: "#0057DA", margin: "1rem", width: "10rem" }}
+				onClick={() => setScrollType("overflowY-scroll")}
 			>
 				LOAD MORE
 			</button>
