079fc6f0804deb2a176408d9b3d9b6a539afb879
Dinesh I <dineshiyyandhurai@gmail.com>
2021-11-17 13:47:33 +0530

    add public page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Home from './Components/Home/home';
 import NotFound from './Components/NotFound/NotFound';
 import Page from './Components/Page/Page';
 import Creation from './Components/Creation/creation';
+import PublicPage from './Components/PublicPage/PublicPage';
+
 function App() {
 	return (
 		<div className='App'>
@@ -13,7 +15,8 @@ function App() {
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route exact path='/create' component={Creation} />
-					<Route exact path='/page/:id' render={props => <Page {...props} />} />
+					<Route exact path='/page' render={(props) => <PublicPage {...props} />} />
+					<Route exact path='/page/:id' render={(props) => <Page {...props} />} />
 					<Route component={NotFound} />
 				</Switch>
 			</Router>
